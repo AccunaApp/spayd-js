61b6e372159d2a581fc0ef4a2d6035d321990475
Tomas Heky Hekrla <24366574+hekystyle@users.noreply.github.com>
2025-12-30 22:19:13 +0100

    release: v3.0.5

---
+++ dist/types/PaymentDescription.d.ts
@@ -0,0 +1,17 @@
+export default interface PaymentDescription {
+    acc: string;
+    altAcc?: string[];
+    am: string;
+    cc?: string | undefined;
+    rf?: number | undefined;
+    rn?: string | undefined;
+    dt?: Date | undefined;
+    pt?: string | undefined;
+    msg?: string | undefined;
+    crc32?: string | undefined;
+    xper?: string | undefined;
+    xvs?: string | undefined;
+    xss?: string | undefined;
+    xks?: string | undefined;
+    xid?: string | undefined;
+}
